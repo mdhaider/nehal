52206365309eba693d4a8900a61d2d12edbd0ca4
Nehaluddin Haider <nehaluddin.haider@dish.com>
2022-12-06 21:25:47 +0530

    minor change to test1

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,50 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const list = [
+  {
+    id: "1",
+    name: "Android",
+  },
+  {
+    id: "2",
+    name: "Java",
+  },
+  {
+    id: "3",
+    name: "Kotlin",
+  },
+  {
+    id: "4",
+    name: "Javascript",
+  },
+  {
+    id: "5",
+    name: "Typescript",
+  },
+  {
+    id: "6",
+    name: "React Native",
+  },
+  {
+    id: "7",
+    name: "Design Pattern",
+  },
+  {
+    id: "8",
+    name: "Data Structures",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ul>
+          {list.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
         <p>Welcome Nehal</p>
       </header>
     </div>
